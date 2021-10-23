604612a1be4c351b41e83917be2508aba4c0c5cc
Teja9493 <prudhvitheja2000@gmail.com>
2021-10-23 19:03:28 +0530

    Second commit

--- scrapenews/scrapenews/urls.py
+++ scrapenews/scrapenews/urls.py
@@ -19,12 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
-from django.urls import path, include
 admin.autodiscover()
 
 urlpatterns = [
     path('',include('scrapetechnews.urls')),
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
